b3b80408381b3c7a69978870ce14bd2239fb140e
Gabriel LimYH <gab.on.rails@gmail.com>
2012-09-27 09:41:08 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySpreeApp::Application.config.secret_token = '05ca2f0425d1cc99565deec7656da6902c835ae2a6cdb95adda33451b6910a80e21c08e26030cb4c343c8c51a9d92eae1921a2b4cb4e3b398066fffdf517d6cb'
